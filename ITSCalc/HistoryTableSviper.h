5e08bab21106038a230fa884ab061f359341de91
Sychov Intencom <sychovintencom@MacBook-Air-Sychov.local>
2014-08-19 20:10:47 +0300

    set up changeable swiper just start need to do more

--- ITSCalc/HistoryTableSviper.h
+++ ITSCalc/HistoryTableSviper.h
@@ -9,5 +9,6 @@
 #import <UIKit/UIKit.h>
 
 @interface HistoryTableSviper : UIView
+@property (nonatomic) BOOL isShowedButtom;
 
 @end
--- ITSCalc/HistoryTableSviper.m
+++ ITSCalc/HistoryTableSviper.m
@@ -14,21 +14,35 @@
 {
     self = [super initWithFrame:frame];
     if (self) {
+        self.isShowedButtom = NO;
         // Initialization code
     }
     return self;
 }
 
+-(void) setIsShowedButtom:(BOOL)isShowedButtom
+{
+    _isShowedButtom = isShowedButtom;
+    [self setNeedsDisplay];
+}
 
 -(void) drawSwipeSymbolInContext:(CGContextRef) context
 {
-    CGRect rect = self.frame;
-   
+    CGFloat thirdPart = self.frame.size.height/3;
+    CGRect rect = CGRectMake(self.frame.origin.x,
+                             self.frame.origin.y + thirdPart,
+                             self.frame.size.width,
+                             thirdPart);
+    
     CGFloat lineWith = rect.size.height/4;
     CGContextSetLineWidth(context, lineWith);
-    CGContextMoveToPoint(context, rect.size.width/6 +.5, rect.size.height /2 +.5 );
-    CGContextAddLineToPoint(context, rect.size.width/2 +.5, rect.size.height - ( lineWith) +.5);
-    CGContextAddLineToPoint(context, rect.size.width *5/6 +.5, rect.size.height /2 +.5);
+    CGContextMoveToPoint(context, rect.size.width/6 +.5, rect.size.height /2 +.5 + thirdPart);
+    if(self.isShowedButtom){
+        CGContextAddLineToPoint(context, rect.size.width/2 +.5,/* rect.size.height */ + (lineWith) +.5 + thirdPart);
+    } else {
+        CGContextAddLineToPoint(context, rect.size.width/2 +.5, rect.size.height - (lineWith) +.5 + thirdPart);
+    }
+    CGContextAddLineToPoint(context, rect.size.width *5/6 +.5, rect.size.height /2 +.5 + thirdPart);
     CGContextSetLineCap(context , kCGLineCapRound);
     CGContextSetRGBStrokeColor(context, 0, 0, 0, 0.3);
     CGContextStrokePath(context);  
