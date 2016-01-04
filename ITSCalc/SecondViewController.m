594d64bd662111ba80fa84a60015e0a4d4dee5a7
я <sychov@rbcmail.ru>
2016-01-04 13:42:37 +0300

    deleted not necessary functions setNeedViews and viewLayout

--- ITSCalc/SecondViewController.m
+++ ITSCalc/SecondViewController.m
@@ -57,55 +57,12 @@ animationControllerForDismissedController:(UIViewController *)dismissed
 //avstract functions
 -(void)setNeedViews
 {
-    /*
-    UILabel *label = [[UILabel alloc] init];
-    label.text = @"SECOND VIEW CONTROLLER";
-    label.textAlignment = NSTextAlignmentCenter;
-    label.textColor = [UIColor blackColor];
-    label.font = [UIFont systemFontOfSize:30];
-    label.baselineAdjustment = UIBaselineAdjustmentAlignBaselines;
-    label.adjustsFontSizeToFitWidth = YES;
-    [self.cView addSubview:label];
-    _label = label;
-    
-    UIButton *changeButton = [UIButton buttonWithType:UIButtonTypeSystem];
-    changeButton.contentMode = UIViewContentModeRedraw;
-    changeButton.tintColor = [UIColor whiteColor];
-    [changeButton addTarget:self action:@selector(userPressedChangeButton:) forControlEvents:UIControlEventTouchUpInside];
-    //changeButton.backgroundColor = [UIColor clearColor];
-    [changeButton setTitle:@"Change main controller" forState:UIControlStateNormal];
-    changeButton.titleLabel.font = [UIFont systemFontOfSize:30];
-    changeButton.titleLabel.numberOfLines = 0;
-    changeButton.titleLabel.textAlignment = NSTextAlignmentCenter;
-    [self.cView addSubview:changeButton];
-    _changeButton = changeButton;
-    
-    //continue button
-    UIButton *goBackButton = [UIButton buttonWithType:UIButtonTypeSystem];
-    goBackButton.contentMode = UIViewContentModeRedraw;
-    goBackButton.tintColor = [UIColor whiteColor];
-    [goBackButton addTarget:self action:@selector(userPressedBackButton:) forControlEvents:UIControlEventTouchUpInside];
-    //goBackButton.backgroundColor = [UIColor clearColor];
-    [goBackButton setTitle:@"GO BACK" forState:UIControlStateNormal];
-    goBackButton.titleLabel.font = [UIFont systemFontOfSize:30];
-    goBackButton.titleLabel.numberOfLines = 0;
-    goBackButton.titleLabel.textAlignment = NSTextAlignmentCenter;
-    [self.cView addSubview:goBackButton];
-    _goBackButton = goBackButton;
-    */
+
 }
 
 -(void) viewDidLayoutSubviewsWithRect:(CGRect)rect
 {
-    /*
-    //thre ellements take firsts three parts from four
-    CGFloat part = (rect.size.height - 2*INDENT)/3;
-    CGFloat width = rect.size.width - 2*INDENT;
-    [self.changeButton setFrame:CGRectMake(rect.origin.x+ INDENT,rect.origin.y+ INDENT, width, part)];
-    
-    [self.label setFrame:CGRectMake(rect.origin.x+INDENT, rect.origin.y+INDENT+part, width, part)];
-    [self.goBackButton setFrame:CGRectMake(rect.origin.x+INDENT, rect.origin.y+ INDENT+2*part, width, part)];
-    */
+
 }
 
 #pragma mark VIEW DID LOAD
